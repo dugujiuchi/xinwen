e4c9dae68182cc6a35e8d26ad9285b28aacdee0b
zhenggaofeng <1470qwer@163.com>
2026-05-19 12:44:05 +0800

    标题修改

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
     <header class="app-header">
       <div class="container">
         <h1 class="app-title">
-          <span class="title-full">宁波资规大数据中心 · 资讯聚合</span>
+          <span class="title-full">资讯聚合平台</span>
           <span class="title-short">资讯聚合</span>
         </h1>
       </div>
@@ -22,10 +22,12 @@
 .title-short {
   display: none;
 }
+
 @media (max-width: 480px) {
   .title-full {
     display: none;
   }
+
   .title-short {
     display: inline;
   }
